e9f4d8032e2040347bb00ae9218906b3960e2a19
Lorena <slsandoval.g@gmail.com>
2021-06-09 21:57:24 -0500

    Created items (index) and breadcrums components

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="breadcrumb container">
+		<a v-for="(item, index) in items" :key="index" href="">
+			{{ item.text }}
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Breadcrumb',
+	props: {
+		items: {
+			type: Array,
+			require: true,
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/scss/constants.scss';
+
+.breadcrumb {
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	padding: $ml-space-1-x 0;
+
+	a {
+		position: relative;
+		text-decoration: none;
+		color: $grey-darken-1;
+		font-size: $ml-body-text-m;
+
+		&:last-child {
+			font-weight: bold;
+		}
+
+		&:not(:last-child) {
+			margin-right: $ml-space-1-and-half-x;
+
+			&::after {
+				position: absolute;
+				content: '\203A';
+				right: -$ml-space-1-x;
+			}
+		}
+	}
+}
+</style>
---
+++ pages/items/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<Breadcrumb :items="breadcrumbs" />
+	</div>
+</template>
+
+<script>
+import Breadcrumb from '~/components/Breadcrumb'
+
+export default {
+	name: 'Items',
+	components: {
+		Breadcrumb,
+	},
+	data() {
+		return {
+			breadcrumbs: [
+				{ text: 'Electronica', href: '' },
+				{ text: 'Celulares', href: '' },
+				{ text: 'Iphone', href: '' },
+			],
+		}
+	},
+}
+</script>
