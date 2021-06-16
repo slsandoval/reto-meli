eb15d9255df82d3080a8648b4ad827760da3c7c6
Lorena <slsandoval.g@gmail.com>
2021-06-16 11:59:24 -0500

    Including try catch and error handling

--- endpoints/search.js
+++ endpoints/search.js
@@ -2,10 +2,17 @@ import { BASE_URL } from '~/endpoints/constants'
 import { searchTransform } from '~/transforms/search'
 
 export async function search(query) {
-	const response = await fetch(`${BASE_URL}/sites/MLA/search?q=:${query}`, {
-		method: 'GET',
-	})
-	const data = await response.json()
-	return searchTransform(data)
-	// if (response.status === 200 & )
+	try {
+		const response = await fetch(`${BASE_URL}/sites/MLA/search?q=:${query}`, {
+			method: 'GET',
+		})
+		const data = await response.json()
+		if (response.status === 200 && Object.keys(data).length) {
+			return { data: searchTransform(data), error: false  }
+		} else {
+			throw new Error('No se encontró información')
+		}
+	} catch (error) {
+		return { data: null, error }
+	}
 }
